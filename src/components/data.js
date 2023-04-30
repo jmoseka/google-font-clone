28b6b55b53968873fa9a72df9936f049c381924d
jmoseka <jamylam3@gmail.com>
2023-04-30 11:27:44 +0300

    Add demo fonts

---
+++ src/components/data.js
@@ -0,0 +1,51 @@
+const fontsData = [
+    {
+        id:1,
+        fontname:'Roboto',
+        fontowner:'Christian Robertson',
+        text:'Whereas recognition of the inherit dignity',
+        fontName: 'Roboto',
+        fontfamily: 'sans-serif'
+    },
+
+    {
+        id:2,
+        fontname:'Comme',
+        fontowner:'Vernon Adams',
+        text:'whereas recognition of the inherit dignity',
+        fontName: 'Comme',
+        fontfamily: 'sans-serif'
+    },
+
+    {
+        id:3,
+        fontname:'Open Sans',
+        fontowner:'Steve Matteson',
+        text:'Whereas recognition of the inherit dignity',
+        fontfamily: 'sans-serif'
+    },
+
+    {
+        id:4,
+        fontname:'Montserrat',
+        fontowner:' Julieta Ulanovsky, Sol Matas, Juan Pablo del Peral, Jacques Le Bailly',
+        text:'whereas recognition of the inherit dignity',
+        fontfamily: 'sans-serif'
+    },
+
+    {
+        id:5,
+        fontname:'Lato',
+        fontowner:'Lukasz Dziedzic',
+        text:'whereas recognition of the inherit dignity',
+        fontfamily: 'sans-serif'
+    },
+
+    {
+        id:6,
+        fontname:'Poppins',
+        fontowner:'Indian Type Foundry, Jonny Pinhorn',
+        text:'Whereas recognition of the inherit dignity',
+        fontfamily: 'sans-serif'
+    }
+]
